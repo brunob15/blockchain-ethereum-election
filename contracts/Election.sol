40dcb895856a8a39fdc418e7c763680ec6ed2db8
Bruno Bradach <bruno.bradach@gmail.com>
2020-05-13 23:38:38 -0300

    List candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates count
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
